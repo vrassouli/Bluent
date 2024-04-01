e45f59385a650ddfefb83d9e672511e537024a71
Vahid Rassouli <vrassouli@live.com>
2024-04-01 12:38:30 +0330

    Menu list added

---
+++ src/Bluent.UI/Components/MenuListComponent/MenuItem.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluent.UI.Components;
+
+public partial class MenuItem
+{
+    [Parameter, EditorRequired] public string Title { get; set; } = default!;
+    [CascadingParameter] public MenuList Menu { get; set; } = default!;
+
+    protected override void OnInitialized()
+    {
+        if (Menu == null)
+            throw new InvalidOperationException($"{nameof(MenuItem)} component should be used inside a {nameof(MenuList)} component.");
+
+        base.OnInitialized();
+    }
+
+    public override IEnumerable<string> GetClasses()
+    {
+        yield return "menu-item";
+    }
+}
---
+++ src/Bluent.UI/Components/MenuListComponent/MenuList.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluent.UI.Components;
+
+public partial class MenuList
+{
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+
+    public override IEnumerable<string> GetClasses()
+    {
+        yield return "bui-menu-list";
+    }
+}
