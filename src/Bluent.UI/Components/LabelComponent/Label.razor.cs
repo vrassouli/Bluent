7cabfa7a9043e861e2aa08c4587bb05e01c8ebd5
Vahid Rassouli <vrassouli@live.com>
2024-04-05 22:25:10 +0330

    Label component added

---
+++ src/Bluent.UI/Components/LabelComponent/Label.razor.cs
@@ -0,0 +1,45 @@
+﻿using Bluent.UI.Extensions;
+using Humanizer;
+using Microsoft.AspNetCore.Components;
+using System.Linq.Expressions;
+
+namespace Bluent.UI.Components;
+
+public partial class Label
+{
+    [Parameter] public string? Text { get; set; }
+    [Parameter] public Expression<Func<object>>? For { get; set; }
+    [Parameter] public string? Required { get; set; }
+    [Parameter] public RenderFragment? Info { get; set; }
+    [Parameter] public LabelSize Size { get; set; } = LabelSize.Medium;
+
+    public override IEnumerable<string> GetClasses()
+    {
+        yield return "bui-label";
+
+        if (IsDisabled)
+            yield return "disabled";
+
+        if (Size != LabelSize.Medium)
+            yield return Size.ToString().Kebaberize();
+    }
+
+    private string? GetPropertyName()
+    {
+        if (For == null)
+            return null;
+
+        return For.GetMemberName();
+    }
+
+    private string? GetDisplayName()
+    {
+        if (!string.IsNullOrEmpty(Text))
+            return Text;
+
+        if (For == null)
+            return null;
+
+        return For.GetDisplayName();
+    }
+}
---
+++ src/Bluent.UI/Components/LabelComponent/LabelSize.cs
@@ -0,0 +1,8 @@
+﻿namespace Bluent.UI.Components;
+
+public enum LabelSize
+{
+    Small,
+    Medium,
+    Large
+}
