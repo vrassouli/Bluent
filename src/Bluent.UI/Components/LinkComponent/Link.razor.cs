015dfa6b749687c971f030cfb8c66560e2afce79
Vahid Rassouli <vrassouli@live.com>
2024-04-05 22:25:34 +0330

    Link component added

---
+++ src/Bluent.UI/Components/LinkComponent/Link.razor.cs
@@ -0,0 +1,47 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluent.UI.Components;
+
+public partial class Link
+{
+    [Parameter, EditorRequired] public string Text { get; set; } = default!;
+    [Parameter] public LinkAppearance Appearance { get; set; } = LinkAppearance.Default;
+
+    private string? Href => AdditionalAttributes?.ContainsKey("href") == true ?
+        AdditionalAttributes["href"]?.ToString() : null;
+
+    public override IEnumerable<string> GetClasses()
+    {
+        yield return "bui-link";
+
+        if (Appearance != LinkAppearance.Default)
+            yield return Appearance.ToString().Kebaberize();
+    }
+
+    private Dictionary<string, object>? GetAdditionalAttributes()
+    {
+        if (AdditionalAttributes == null)
+        {
+            return null;
+        }
+        else
+        {
+            var dic = new Dictionary<string, object>();
+            foreach (var item in AdditionalAttributes)
+            {
+                if (item.Key.Equals("href", StringComparison.InvariantCultureIgnoreCase) && IsDisabled)
+                    continue;
+
+                dic.Add(item.Key, item.Value);
+            }
+
+            return dic;
+        }
+    }
+}
---
+++ src/Bluent.UI/Components/LinkComponent/LinkAppearance.cs
@@ -0,0 +1,7 @@
+﻿namespace Bluent.UI.Components;
+
+public enum LinkAppearance
+{
+    Default,
+    Subtle
+}
