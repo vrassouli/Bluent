fcd308c2c1dbbc49be12bb9b91abceb4602ff7c3
Vahid Rassouli <vrassouli@live.com>
2024-04-05 14:15:34 +0330

    Accordion component added

---
+++ src/Bluent.UI/Components/AccordionComponent/Accordion.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Bluent.UI.Components;
+
+public partial class Accordion
+{
+    private List<AccordionPanel> _panels = new();
+
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+    [Parameter] public AccordionHeaderAction HeaderAction { get; set; } = AccordionHeaderAction.Expand;
+    [Parameter] public bool Multiple { get; set; }
+
+    public override IEnumerable<string> GetClasses()
+    {
+        yield return "bui-accordion";
+    }
+
+    internal void Add(AccordionPanel panel)
+    {
+        _panels.Add(panel);
+    }
+
+    internal void Remove(AccordionPanel panel)
+    {
+        _panels.Remove(panel);
+    }
+
+    internal void OnPanelCollapsing(AccordionPanel panel)
+    {
+    }
+
+    internal void OnPanelExpanding(AccordionPanel panel)
+    {
+        if (!Multiple)
+            _panels.FirstOrDefault(x => x.State == AccordionPanelState.Expanded)?.Collapse();
+    }
+}
---
+++ src/Bluent.UI/Components/AccordionComponent/AccordionPanel.razor.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Bluent.UI.Components;
+
+public partial class AccordionPanel
+{
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+    [Parameter, EditorRequired] public string Header { get; set; } = default!;
+    [Parameter] public string HeaderIcon { get; set; } = default!;
+    [Parameter] public AccordionPanelState State { get; set; } = AccordionPanelState.Collapsed;
+    [Parameter] public EventCallback<AccordionPanelState> StateChanged { get; set; }
+    [CascadingParameter] public Accordion Accordion { get; set; } = default!;
+
+    protected override void OnInitialized()
+    {
+        if (Accordion is null)
+            throw new InvalidOperationException($"'{nameof(AccordionPanel)}' component should be nested inside a '{nameof(Components.Accordion)}' component.");
+
+        Accordion.Add(this);
+
+        base.OnInitialized();
+    }
+
+    public override void Dispose()
+    {
+        Accordion.Remove(this);
+
+        base.Dispose();
+    }
+
+    public override IEnumerable<string> GetClasses()
+    {
+        yield return "accordion-panel";
+
+        if (State == AccordionPanelState.Expanded)
+            yield return "expanded";
+    }
+
+    public void Toggle()
+    {
+        if (State == AccordionPanelState.Collapsed)
+            Expand();
+        else
+            Collapse();
+    }
+
+    public void Collapse()
+    {
+        if (State == AccordionPanelState.Collapsed)
+            return;
+
+        Accordion.OnPanelCollapsing(this);
+
+        State = AccordionPanelState.Collapsed;
+        InvokeAsync(() => StateChanged.InvokeAsync(State));
+
+        StateHasChanged();
+    }
+
+    public void Expand()
+    {
+        if (State == AccordionPanelState.Expanded)
+            return;
+        
+        Accordion.OnPanelExpanding(this);
+
+        State = AccordionPanelState.Expanded;
+        InvokeAsync(() => StateChanged.InvokeAsync(State));
+    
+        StateHasChanged();
+    }
+
+    private void ClickHandler()
+    {
+        if (Accordion.HeaderAction == AccordionHeaderAction.Expand)
+            Expand();
+        else
+            Toggle();
+    }
+}
