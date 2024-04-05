b93cba73860bb8039b421da5193efe0bd0d28b92
Vahid Rassouli <vrassouli@live.com>
2024-04-05 14:16:08 +0330

    ButtonGroup component added

---
+++ src/Bluent.UI/Components/ButtonGroupComponent/ButtonGroup.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluent.UI.Components;
+
+public partial class ButtonGroup
+{
+    [Parameter] public RenderFragment? ChildContent { get;set; }
+    public override IEnumerable<string> GetClasses()
+    {
+        yield return "bui-button-group";
+    }
+}
