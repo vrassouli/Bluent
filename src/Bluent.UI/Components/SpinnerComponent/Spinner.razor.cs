6d6041a97363fcfaf4de58d2374d67958c637392
Vahid Rassouli <vrassouli@gmail.com>
2024-04-18 17:24:21 +0330

    Spinner added

---
+++ src/Bluent.UI/Components/SpinnerComponent/Spinner.razor.cs
@@ -0,0 +1,27 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluent.UI.Components;
+
+public partial class Spinner
+{
+    [Parameter] public SpinnerAppearance Appearance { get; set; } = SpinnerAppearance.Primary;
+    [Parameter] public SpinnerLabelPosition LabelPosition { get; set; } = SpinnerLabelPosition.Below;
+    [Parameter] public string? Label { get; set; }
+
+    public override IEnumerable<string> GetClasses()
+    {
+        yield return "bui-spinner";
+
+        if (Appearance != SpinnerAppearance.Primary)
+            yield return Appearance.ToString().Kebaberize();
+
+        if(LabelPosition != SpinnerLabelPosition.Below)
+            yield return LabelPosition.ToString().Kebaberize();
+    }
+}
