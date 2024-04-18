fed0387198a1797b85698246c32ef792b9bf3dab
Vahid Rassouli <vrassouli@live.com>
2024-04-18 22:52:12 +0330

    Spinner size added

--- src/Bluent.UI/Components/SpinnerComponent/Spinner.razor.cs
+++ src/Bluent.UI/Components/SpinnerComponent/Spinner.razor.cs
@@ -12,6 +12,7 @@ public partial class Spinner
 {
     [Parameter] public SpinnerAppearance Appearance { get; set; } = SpinnerAppearance.Primary;
     [Parameter] public SpinnerLabelPosition LabelPosition { get; set; } = SpinnerLabelPosition.Below;
+    [Parameter] public SpinnerSize Size { get; set; } = SpinnerSize.Medium;
     [Parameter] public string? Label { get; set; }
 
     public override IEnumerable<string> GetClasses()
@@ -21,7 +22,10 @@ public partial class Spinner
         if (Appearance != SpinnerAppearance.Primary)
             yield return Appearance.ToString().Kebaberize();
 
-        if(LabelPosition != SpinnerLabelPosition.Below)
+        if (LabelPosition != SpinnerLabelPosition.Below)
             yield return LabelPosition.ToString().Kebaberize();
+
+        if (Size != SpinnerSize.Medium)
+            yield return Size.ToString().Kebaberize();
     }
 }
---
+++ src/Bluent.UI/Components/SpinnerComponent/SpinnerSize.cs
@@ -0,0 +1,13 @@
+﻿namespace Bluent.UI.Components;
+
+public enum SpinnerSize
+{
+    ExtraTiny,
+    Tiny,
+    ExtraSmall,
+    Small,
+    Medium,
+    Large,
+    ExtraLarge,
+    Huge
+}
