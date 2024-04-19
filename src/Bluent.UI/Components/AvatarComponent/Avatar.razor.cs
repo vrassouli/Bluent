4c1496db5093e922710878653b975b842be18293
Vahid Rassouli <vrassouli@live.com>
2024-04-20 01:39:51 +0330

    feat: Avatar component added

---
+++ src/Bluent.UI/Components/AvatarComponent/Avatar.razor.cs
@@ -0,0 +1,40 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluent.UI.Components;
+
+public partial class Avatar
+{
+    [Parameter] public string? Initials { get; set; }
+    [Parameter] public string? Name { get; set; }
+    [Parameter] public string? ImageSource { get; set; }
+    [Parameter] public string? Icon { get; set; }
+    [Parameter] public AvatarSize Size { get; set; } = AvatarSize.Size32;
+
+    private string? DisplayInitial
+    {
+        get
+        {
+            if(!string.IsNullOrEmpty(Initials))
+                return Initials;
+
+            if(!string.IsNullOrEmpty(Name)) 
+                return string.Concat(Name.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => x[0]));
+            
+            return null;
+        }
+    }
+
+    public override IEnumerable<string> GetClasses()
+    {
+        yield return "bui-avatar";
+
+        if (Size != AvatarSize.Size32)
+            yield return Size.ToString().Kebaberize();
+    }
+}
---
+++ src/Bluent.UI/Components/AvatarComponent/AvatarSize.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluent.UI.Components;
+
+public enum AvatarSize
+{
+    Size16,
+    Size20,
+    Size24,
+    Size28,
+    Size32,
+    Size36,
+    Size40,
+    Size48,
+    Size56,
+    Size64,
+    Size72,
+    Size96,
+    Size120,
+    Size128,
+}
