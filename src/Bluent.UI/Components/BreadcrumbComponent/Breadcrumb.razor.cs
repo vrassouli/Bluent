b0c0d531c9f4c3db13ce32ca8ce8fa6f30e4bf96
Vahid Rassouli <vrassouli@gmail.com>
2024-04-21 18:31:11 +0330

    feat: Breadcrumb added

---
+++ src/Bluent.UI/Components/BreadcrumbComponent/Breadcrumb.razor.cs
@@ -0,0 +1,18 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Components;
+
+namespace Bluent.UI.Components;
+
+public partial class Breadcrumb
+{
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+    [Parameter] public BreadcrumbSize Size { get; set; } = BreadcrumbSize.Medium;
+
+    public override IEnumerable<string> GetClasses()
+    {
+        yield return "bui-breadcrumb";
+
+        if (Size != BreadcrumbSize.Medium)
+            yield return Size.ToString().Kebaberize();
+    }
+}
