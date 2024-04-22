94f9d3401f0158aa4970986e060f7be9988eb4f0
Vahid Rassouli <vrassouli@live.com>
2024-04-23 00:11:21 +0330

    feat: Tree added

---
+++ src/Bluent.UI/Components/TreeComponent/Tree.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Bluent.UI.Components;
+
+public partial class Tree
+{
+    private List<TreeItem> _items = new();
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+    [Parameter] public EventCallback<TreeItem> OnClick { get; set; }
+
+    //internal bool ChildrenHaveSubItems => _items.Any(i => i.HaveSubItems);
+
+    public override IEnumerable<string> GetClasses()
+    {
+        yield return "bui-tree";
+    }
+
+    internal void Add(TreeItem item)
+    {
+        _items.Add(item);
+        StateHasChanged();
+    }
+
+    internal void Remove(TreeItem item)
+    {
+        _items.Remove(item);
+        StateHasChanged();
+    }
+
+    internal void OnItemClick(TreeItem item)
+    {
+        OnClick.InvokeAsync(item);
+    }
+}
---
+++ src/Bluent.UI/Components/TreeComponent/TreeItem.razor.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluent.UI.Components;
+
+public partial class TreeItem
+{
+    private List<TreeItem> _items = new();
+
+    [Parameter, EditorRequired] public string Title { get; set; } = default!;
+    [Parameter] public string? Icon { get; set; } = default!;
+    [Parameter] public string? ExpandedIcon { get; set; } = default!;
+    [Parameter] public bool Expanded { get; set; } = default!;
+    [Parameter] public EventCallback<bool> ExpandedChanged { get; set; } = default!;
+    [Parameter] public bool? IsChecked { get; set; } = default!;
+    [Parameter] public EventCallback<bool?> IsCheckedChanged { get; set; } = default!;
+    [Parameter] public object? Data { get; set; } = default!;
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+    [CascadingParameter] public Tree Tree { get; set; } = default!;
+    [CascadingParameter] public TreeItem? ParentItem { get; set; } = default!;
+
+    internal bool HasSubItems => _items.Any();
+    //private bool ShouldRenderExpander
+    //{
+    //    get
+    //    {
+    //        if (HaveSubItems)
+    //            return true;
+    //        else if (ParentItem != null)
+    //            return ParentItem.ChildrenHaveSubItems;
+    //        else
+    //            return Tree.ChildrenHaveSubItems;
+    //    }
+    //}
+
+    //private bool ChildrenHaveSubItems => _items.Any(i => i.HaveSubItems);
+
+    protected override void OnInitialized()
+    {
+        if (Tree is null)
+            throw new InvalidOperationException($"'{nameof(TreeItem)}' component should be nested inside a '{nameof(Components.Tree)}' or '{nameof(TreeItem)}' component.");
+
+        if (ParentItem != null)
+            ParentItem.Add(this);
+        else
+            Tree.Add(this);
+
+        base.OnInitialized();
+    }
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (firstRender)
+            StateHasChanged();
+
+        base.OnAfterRender(firstRender);
+    }
+
+    public override void Dispose()
+    {
+        if (ParentItem != null)
+            ParentItem.Remove(this);
+        else
+            Tree.Remove(this);
+
+        base.Dispose();
+    }
+
+    public override IEnumerable<string> GetClasses()
+    {
+        yield return "item";
+
+        if (Expanded)
+            yield return "expanded";
+    }
+
+    private void Add(TreeItem item)
+    {
+        _items.Add(item);
+        //StateHasChanged();
+    }
+
+    private void Remove(TreeItem item)
+    {
+        _items.Remove(item);
+        //StateHasChanged();
+    }
+
+    private void ClickHandler()
+    {
+        if (HasSubItems)
+        {
+            Expanded = !Expanded;
+            ExpandedChanged.InvokeAsync(Expanded);
+        }
+
+        Tree.OnItemClick(this);
+    }
+}
