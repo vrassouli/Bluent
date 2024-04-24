4325379aae3f285688b8d9d5c3dd9dda6e4db969
Vahid Rassouli <vrassouli@live.com>
2024-04-25 01:02:39 +0330

    feat: Tag component Added

---
+++ src/Bluent.UI/Components/TagComponent/Tag.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluent.UI.Components;
+
+public partial class Tag
+{
+    [Parameter] public string Title { get; set; } = default!;
+    [Parameter] public bool Dismissable { get; set; }
+    [Parameter] public string? Icon { get; set; }
+    [Parameter] public EventCallback OnClick { get; set; }
+    [Parameter] public EventCallback OnDismiss { get; set; }
+    
+    public override IEnumerable<string> GetClasses()
+    {
+        yield return "bui-tag";
+    }
+}
