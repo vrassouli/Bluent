357e460ca46d4d1a5c23f2812a903bc284128e3e
Vahid Rassouli <vrassouli@gmail.com>
2024-04-27 15:02:34 +0330

    feat: MessageBar added

---
+++ src/Bluent.UI/Components/MessageBarComponent/MessageBar.razor.cs
@@ -0,0 +1,41 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluent.UI.Components;
+
+public partial class MessageBar
+{
+    [Parameter] public MessageBarType Type { get; set; } = MessageBarType.Default;
+    [Parameter] public bool Dismissable { get; set; }
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+    [Parameter] public RenderFragment? Actions { get; set; }
+    [Parameter] public bool Multiline { get; set; }
+    [Parameter] public EventCallback OnDismiss { get; set; }
+
+    public override IEnumerable<string> GetClasses()
+    {
+        yield return "bui-message-bar";
+
+        if (Multiline)
+            yield return "multiline";
+
+        if (Type != MessageBarType.Default)
+            yield return Type.ToString().Kebaberize();
+    }
+
+    private string GetIconClass()
+    {
+        return Type switch
+        {
+            MessageBarType.Warning => "icon-ic_fluent_warning_20_filled",
+            MessageBarType.Danger => "icon-ic_fluent_info_20_filled",
+            MessageBarType.Success => "icon-ic_fluent_checkmark_circle_20_filled",
+            _ => "icon-ic_fluent_info_20_filled"
+        };
+    }
+}
