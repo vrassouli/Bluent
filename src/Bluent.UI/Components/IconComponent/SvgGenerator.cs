95ce567d7967cd8a15e4f1c21b9e2b80e6565401
Vahid Rassouli <vrassouli@gmail.com>
2024-04-28 13:34:32 +0330

    feat: SvgGenerator

---
+++ src/Bluent.UI/Components/IconComponent/SvgGenerator.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluent.UI.Components.IconComponent;
+
+public class SvgGenerator
+{
+    private string? _content;
+    private int? _elementWidth;
+    private int? _elementHeight;
+    private int? _viewboxWidth;
+    private int? _viewboxHeight;
+
+    public SvgGenerator Content(string content)
+    {
+        _content = content;
+
+        return this;
+    }
+
+    public SvgGenerator Size(int size)
+    {
+        _elementWidth = size;
+        _elementHeight = size;
+
+        return this;
+    }
+
+    public SvgGenerator Width(int width)
+    {
+        _elementWidth = width;
+
+        return this;
+    }
+
+    public SvgGenerator Height(int height)
+    {
+        _elementHeight = height;
+
+        return this;
+    }
+
+    public SvgGenerator Viewbox(int size)
+    {
+        _viewboxWidth = size;
+        _viewboxHeight = size;
+
+        return this;
+    }
+
+    public SvgGenerator ViewboxWidth(int width)
+    {
+        _viewboxWidth = width;
+
+        return this;
+    }
+
+    public SvgGenerator ViewboxHeight(int height)
+    {
+        _viewboxHeight = height;
+
+        return this;
+    }
+
+    public override string ToString()
+    {
+        return Generate();
+    }
+
+    public static implicit operator string(SvgGenerator self)
+    {
+        return self.ToString();
+    }
+
+    private string Generate()
+    {
+        var w = _elementWidth ?? 36;
+        var h = _elementHeight ?? 36;
+
+        var vbw = _viewboxWidth ?? 36;
+        var vbh = _viewboxHeight ?? 36;
+
+        return $"<svg viewbox=\"0 0 {vbw} {vbh}\" width=\"{w}\" height=\"{h}\">{_content}</svg>";
+    }
+}
