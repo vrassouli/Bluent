d04bd1ef9d0e78a719b7af235fd39fa7b5708068
Vahid Rassouli <vrassouli@gmail.com>
2024-05-18 22:47:48 +0330

    Textarea added

--- src/Bluent.UI/Components/TextFieldComponent/TextField.razor.cs
+++ src/Bluent.UI/Components/TextFieldComponent/TextField.razor.cs
@@ -5,12 +5,18 @@ namespace Bluent.UI.Components;
 
 public partial class TextField
 {
+    [Parameter] public int? Rows { get; set; }
+    [Parameter] public bool ResizeTextarea { get; set; }
+
     public override IEnumerable<string> GetClasses()
     {
         foreach (var c in base.GetClasses())
             yield return c;
 
         yield return "bui-text-field";
+
+        if (Rows != null && !ResizeTextarea)
+            yield return "no-resize";
     }
 
     protected override bool TryParseValueFromString(string? value, out string? result, [NotNullWhen(false)] out string? validationErrorMessage)
