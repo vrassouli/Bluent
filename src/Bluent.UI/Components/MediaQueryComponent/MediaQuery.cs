0984b25fca8bb8ea6aa23b2fd70afa1cae9acd72
Vahid Rassouli <vrassouli@live.com>
2025-05-28 18:01:04 +0330

    Added MediaQuery Component Added IDomHelper Updated service registration

---
+++ src/Bluent.UI/Components/MediaQueryComponent/MediaQuery.cs
@@ -0,0 +1,35 @@
+﻿using Bluent.UI.Services.Abstractions;
+using Microsoft.AspNetCore.Components;
+
+namespace Bluent.UI.Components;
+
+internal class MediaQuery : ComponentBase
+{
+    [Inject] public IDomHelper DomHelper { get; set; } = null!;
+    [Parameter] public EventCallback<Breakpoints> OnChange { get; set; }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await SendInitialBreakpointAsync();
+        }
+
+        await base.OnAfterRenderAsync(firstRender);
+    }
+
+    private async Task SendInitialBreakpointAsync()
+    {
+        foreach (var breakpoint in Enum.GetValues<Breakpoints>().OrderByDescending(x => x))
+        {
+            var query = $"(min-width: {(int)breakpoint}px)";
+
+            var matches = await DomHelper.MatchMediaAsync(query);
+            if (matches)
+            {
+                await OnChange.InvokeAsync(breakpoint);
+                return;
+            }
+        }
+    }
+}
--- src/Bluent.UI/Extensions/ServiceCollectionExtensions.cs
+++ src/Bluent.UI/Extensions/ServiceCollectionExtensions.cs
@@ -9,99 +9,26 @@ namespace Bluent.UI.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    #region Scoped Service
-
-    public static IServiceCollection AddBluentUI(this IServiceCollection services)
+    public static IServiceCollection AddBluentUI(this IServiceCollection services, ServiceLifetime lifetime = ServiceLifetime.Scoped)
     {
         services.AddLocalization();
-
-        AddBluentThemeAsScoped(services);
-        AddBluentDialogServiceAsScoped(services);
-        AddBluentDrawerServiceAsScoped(services);
-        AddBluentPopoverServiceAsScoped(services);
-        AddBluentToastServiceAsScoped(services);
-        AddBluentTooltipServiceAsScoped(services);
+        services.AddService<IBluentTheme, BluentTheme>(lifetime);
+        services.AddService<IDomHelper, DomHelper>(lifetime);
+        services.AddService<IDialogService, DialogService>(lifetime);
+        services.AddService<IDrawerService, DrawerService>(lifetime);
+        services.AddService<IPopoverService, PopoverService>(lifetime);
+        services.AddService<IToastService, ToastService>(lifetime);
+        services.AddService<ITooltipService, TooltipService>(lifetime);
 
         return services;
     }
 
-    public static void AddBluentTooltipServiceAsScoped(IServiceCollection services)
+    public static void AddService<TService, TImplementation>(this IServiceCollection services, ServiceLifetime lifetime)
+        where TService : class 
+        where TImplementation : class, TService
     {
-        services.AddScoped<ITooltipService, TooltipService>();
+        var item = new ServiceDescriptor(typeof(TService), typeof(TImplementation), lifetime);
+
+        services.Add(item);
     }
-
-    public static void AddBluentToastServiceAsScoped(IServiceCollection services)
-    {
-        services.AddScoped<IToastService, ToastService>();
-    }
-
-    public static void AddBluentPopoverServiceAsScoped(IServiceCollection services)
-    {
-        services.AddScoped<IPopoverService, PopoverService>();
-    }
-
-    public static void AddBluentDrawerServiceAsScoped(IServiceCollection services)
-    {
-        services.AddScoped<IDrawerService, DrawerService>();
-    }
-
-    public static void AddBluentDialogServiceAsScoped(IServiceCollection services)
-    {
-        services.AddScoped<IDialogService, DialogService>();
-    }
-
-    public static void AddBluentThemeAsScoped(IServiceCollection services)
-    {
-        services.AddScoped<IBluentTheme, BluentTheme>();
-    }
-
-    #endregion
-
-    #region Singleton Service
-
-    public static IServiceCollection AddBluentUIAsSingleton(this IServiceCollection services)
-    {
-        services.AddLocalization();
-
-        AddBluentThemeAsSingleton(services);
-        AddBluentDialogServiceAsSingleton(services);
-        AddBluentDrawerServiceAsSingleton(services);
-        AddBluentPopoverServiceAsSingleton(services);
-        AddBluentToastServiceAsSingleton(services);
-        AddBluentTooltipServiceAsSingleton(services);
-
-        return services;
-    }
-
-    public static void AddBluentTooltipServiceAsSingleton(IServiceCollection services)
-    {
-        services.AddSingleton<ITooltipService, TooltipService>();
-    }
-
-    public static void AddBluentToastServiceAsSingleton(IServiceCollection services)
-    {
-        services.AddSingleton<IToastService, ToastService>();
-    }
-
-    public static void AddBluentPopoverServiceAsSingleton(IServiceCollection services)
-    {
-        services.AddSingleton<IPopoverService, PopoverService>();
-    }
-
-    public static void AddBluentDrawerServiceAsSingleton(IServiceCollection services)
-    {
-        services.AddSingleton<IDrawerService, DrawerService>();
-    }
-
-    public static void AddBluentDialogServiceAsSingleton(IServiceCollection services)
-    {
-        services.AddSingleton<IDialogService, DialogService>();
-    }
-
-    public static void AddBluentThemeAsSingleton(IServiceCollection services)
-    {
-        services.AddSingleton<IBluentTheme, BluentTheme>();
-    }
-
-    #endregion
 }
