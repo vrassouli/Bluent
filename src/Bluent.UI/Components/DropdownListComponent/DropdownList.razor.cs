ee01a6282f93705596e0a8408d0426d61f7e99ae
Vahid Rassouli <vrassouli@live.com>
2025-09-21 11:49:29 +0330

    Improved Dropdowns.razor component

--- src/Bluent.UI/Components/DropdownListComponent/DropdownList.razor.cs
+++ src/Bluent.UI/Components/DropdownListComponent/DropdownList.razor.cs
@@ -4,14 +4,14 @@ using Microsoft.Extensions.Localization;
 
 namespace Bluent.UI.Components;
 
-
 public partial class DropdownList<TItem, TValue>
     where TItem : class
 {
+    private TValue? _value;
     private DropdownSelect<TValue>? _dropdown;
     private Virtualize<TItem>? _virtualizer;
     private string? _filter;
-    private List<TItem> _selectedItems = new();
+    private readonly List<TItem> _selectedItems = new();
 
 
     [Parameter] public Placement DropdownPlacement { get; set; } = Placement.BottomStart;
@@ -29,8 +29,9 @@ public partial class DropdownList<TItem, TValue>
     [Parameter] public float ItemSize { get; set; } = 50;
     [Parameter, EditorRequired] public Func<TItem, TValue?> ItemValue { get; set; } = _ => default;
     [Parameter, EditorRequired] public Func<TItem?, string> ItemText { get; set; } = default!;
-    [Parameter, EditorRequired] public RenderFragment<TItem> ItemContent { get; set; } = default!;
+    [Parameter] public RenderFragment<TItem>? ItemContent { get; set; }
     [Parameter, EditorRequired] public FilteredItemsProviderDelegate<TItem> ItemsProvider { get; set; } = default!;
+    [Parameter, EditorRequired] public Func<TValue, TItem> ItemProvider { get; set; } = default!;
     [Parameter] public RenderFragment<PlaceholderContext>? Placeholder { get; set; }
     [Parameter] public RenderFragment? EmptyContent { get; set; }
     [Inject] private IStringLocalizer<DropdownListComponent.Resources.DropdownList> Localizer { get; set; } = default!;
@@ -75,16 +76,41 @@ public partial class DropdownList<TItem, TValue>
         base.OnParametersSet();
     }
 
+    protected override async Task OnParametersSetAsync()
+    {
+        if (!Equals(_value, Value))
+        {
+            _value = Value;
+            await OnValueChanged();
+        }
+
+        await base.OnParametersSetAsync();
+    }
+
+    private async Task OnValueChanged()
+    {
+        if (!ValueChanged.HasDelegate)
+            return;
+
+        // User has used @bind-Value on the component
+        // So he is intending to use it in single-select mode
+        // If value is changed from outside the component, we should try to resolve the selection
+        if (Value is null)
+            await ClearSelection();
+        else
+        {
+            var item = ItemProvider.Invoke(Value);
+            await AddToSelections(item);
+        }
+    }
+
     private ValueTask<ItemsProviderResult<TItem>> ListItemsProvider(ItemsProviderRequest request)
-        => ItemsProvider(new FilteredItemsProviderRequest(request.StartIndex, request.Count, _filter, request.CancellationToken));
+        => ItemsProvider(new FilteredItemsProviderRequest(request.StartIndex, request.Count, _filter,
+            request.CancellationToken));
 
     private bool IsSelected(TItem item)
     {
         return _selectedItems.Any(i => ItemValue(i)?.Equals(ItemValue(item)) ?? false);
-        //if (Value is null)
-        //    return false;
-
-        //return Value.Equals(GetItemValue(item));
     }
 
     private async Task OnItemSelectionChanged(TItem item, bool selected)
@@ -111,6 +137,13 @@ public partial class DropdownList<TItem, TValue>
     {
         foreach (var item in items)
             _selectedItems.Remove(item);
+
+        await InvokeChangeEvents();
+    }
+
+    private async Task ClearSelection()
+    {
+        _selectedItems.Clear();
 
         await InvokeChangeEvents();
     }
