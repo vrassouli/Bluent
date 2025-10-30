ac284226c97de19d5a089e75e36b5b20aacde9e5
Vahid Rassouli <vrassouli@live.com>
2025-10-30 12:03:50 +0330

    Enhance NumericField component by adding Min and Max parameters for value validation and improving error messaging

--- src/Bluent.UI/Components/NumericFieldComponent/NumericField.razor.cs
+++ src/Bluent.UI/Components/NumericFieldComponent/NumericField.razor.cs
@@ -9,11 +9,17 @@ public partial class NumericField<TValue>
     private string? _userValue;
 
     /// <summary>
-    /// Gets or sets the error message used when displaying an a parsing error.
+    /// Gets or sets the error message used when displaying a parsing error.
     /// </summary>
-    [Parameter] public string ParsingErrorMessage { get; set; } = "The {0} field must be a number.";
+    [Parameter]
+    public string ParsingErrorMessage { get; set; } = "The {0} field must be a number.";
+
+    [Parameter] public string MinimumErrorMessage { get; set; } = "The {0} field must be greater than {1}.";
+    [Parameter] public string MaximumErrorMessage { get; set; } = "The {0} field must be less than {1}.";
     [Parameter] public bool GainFocus { get; set; }
     [Parameter] public string? Format { get; set; }
+    [Parameter] public TValue? Min { get; set; }
+    [Parameter] public TValue? Max { get; set; }
 
     private string? UserValue
     {
@@ -21,7 +27,7 @@ public partial class NumericField<TValue>
         {
             if (_userValue is null)
                 return CurrentValueAsString;
-            
+
             return _userValue;
         }
         set
@@ -38,46 +44,72 @@ public partial class NumericField<TValue>
 
         yield return "bui-numeric-field";
     }
+
     protected override Task OnAfterRenderAsync(bool firstRender)
     {
         if (firstRender && GainFocus && Element != null)
         {
             InvokeAsync(() => Element.Value.FocusAsync());
         }
+
         return base.OnAfterRenderAsync(firstRender);
     }
 
-    private static string GetStepAttributeValue()
-    {
-        // Unwrap Nullable<T>, because InputBase already deals with the Nullable aspect
-        // of it for us. We will only get asked to parse the T for nonempty inputs.
-        var targetType = Nullable.GetUnderlyingType(typeof(TValue)) ?? typeof(TValue);
-        if (targetType == typeof(int) ||
-            targetType == typeof(long) ||
-            targetType == typeof(short) ||
-            targetType == typeof(float) ||
-            targetType == typeof(double) ||
-            targetType == typeof(decimal))
-        {
-            return "any";
-        }
-        else
-        {
-            throw new InvalidOperationException($"The type '{targetType}' is not a supported numeric type.");
-        }
-    }
+    // private static string GetStepAttributeValue()
+    // {
+    //     // Unwrap Nullable<T>, because InputBase already deals with the Nullable aspect
+    //     // of it for us. We will only get asked to parse the T for nonempty inputs.
+    //     var targetType = Nullable.GetUnderlyingType(typeof(TValue)) ?? typeof(TValue);
+    //     if (targetType == typeof(int) ||
+    //         targetType == typeof(long) ||
+    //         targetType == typeof(short) ||
+    //         targetType == typeof(float) ||
+    //         targetType == typeof(double) ||
+    //         targetType == typeof(decimal))
+    //     {
+    //         return "any";
+    //     }
+    //     else
+    //     {
+    //         throw new InvalidOperationException($"The type '{targetType}' is not a supported numeric type.");
+    //     }
+    // }
 
     /// <inheritdoc />
-    protected override bool TryParseValueFromString(string? value, [MaybeNullWhen(false)] out TValue result, [NotNullWhen(false)] out string? validationErrorMessage)
+    protected override bool TryParseValueFromString(string? value, [MaybeNullWhen(false)] out TValue result,
+        [NotNullWhen(false)] out string? validationErrorMessage)
     {
-        if (BindConverter.TryConvertTo<TValue>(value, CultureInfo.InvariantCulture, out result))
+        if (BindConverter.TryConvertTo<TValue>(value, CultureInfo.InvariantCulture, out var tempResult))
         {
             validationErrorMessage = null;
+
+            // Check minimum
+            if (Min is not null && Comparer<TValue>.Default.Compare(tempResult, Min) < 0)
+            {
+                validationErrorMessage = string.Format(CultureInfo.InvariantCulture, MinimumErrorMessage,
+                    DisplayName ?? FieldIdentifier.FieldName, Min);
+                result = default!;
+                return false;
+            }
+
+            // Check maximum
+            if (Max is not null && Comparer<TValue>.Default.Compare(tempResult, Max) > 0)
+            {
+                validationErrorMessage = string.Format(CultureInfo.InvariantCulture, MaximumErrorMessage,
+                    DisplayName ?? FieldIdentifier.FieldName, Max);
+                result = default!;
+                return false;
+            }
+
+            result = tempResult;
             return true;
         }
         else
         {
-            validationErrorMessage = string.Format(CultureInfo.InvariantCulture, ParsingErrorMessage, DisplayName ?? FieldIdentifier.FieldName);
+            validationErrorMessage = string.Format(CultureInfo.InvariantCulture, ParsingErrorMessage,
+                DisplayName ?? FieldIdentifier.FieldName);
+            result = default!;
+
             return false;
         }
     }
@@ -90,9 +122,7 @@ public partial class NumericField<TValue>
     /// <returns>A string representation of the value.</returns>
     protected override string? FormatValueAsString(TValue? value)
     {
-        string? valueString = null;
-
-        valueString = value switch
+        var valueString = value switch
         {
             null => null,
             int v => v.ToString(Format, CultureInfo.CurrentUICulture),
@@ -103,26 +133,6 @@ public partial class NumericField<TValue>
             decimal v => v.ToString(Format, CultureInfo.CurrentUICulture),
             _ => throw new InvalidOperationException($"Unsupported type {value.GetType()}")
         };
-
-
-        //// Avoiding a cast to IFormattable to avoid boxing.
-        //valueString = value switch
-        //{
-        //    null => null,
-        //    int @int => BindConverter.FormatValue(@int, CultureInfo.InvariantCulture),
-        //    long @long => BindConverter.FormatValue(@long, CultureInfo.InvariantCulture),
-        //    short @short => BindConverter.FormatValue(@short, CultureInfo.InvariantCulture),
-        //    float @float => BindConverter.FormatValue(@float, CultureInfo.InvariantCulture),
-        //    double @double => BindConverter.FormatValue(@double, CultureInfo.InvariantCulture),
-        //    decimal @decimal => BindConverter.FormatValue(@decimal, CultureInfo.InvariantCulture),
-        //    _ => throw new InvalidOperationException($"Unsupported type {value.GetType()}")
-        //};
-
-        //if (TrimTrailingZeros && valueString != null)
-        //{
-        //    // Trim trailing zeroes and decimal point if necessary
-        //    valueString = valueString.TrimEnd('0').TrimEnd('.');
-        //}
 
         return valueString;
     }
