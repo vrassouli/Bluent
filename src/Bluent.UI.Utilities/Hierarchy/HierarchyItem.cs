8df3ee7cc87af40fb4fd43b3a2b3fc445eeb4326
Vahid Rassouli <vrassouli@live.com>
2026-05-05 13:35:30 +0330

    Added data to items

--- src/Bluent.UI.Utilities/Hierarchy/HierarchyItem.cs
+++ src/Bluent.UI.Utilities/Hierarchy/HierarchyItem.cs
@@ -3,9 +3,11 @@ namespace Bluent.UI.Utilities;
 public class HierarchyItem
 {
     public string Name { get; set; }
+    public object? Data { get; set; }
 
-    public HierarchyItem(string name)
+    public HierarchyItem(string name, object? data = null)
     {
         Name = name;
+        Data = data;
     }
 }
