42b98d77de790f3ead0a9eeec7b6bde70a55afb3
vahid <vrassouli@gmail.com>
2026-05-29 00:22:55 +0330

    improved MdiTabToolbarItem

--- src/Bluent.UI.Utilities/MdiTab/MdiTabToolbarItem.razor.cs
+++ src/Bluent.UI.Utilities/MdiTab/MdiTabToolbarItem.razor.cs
@@ -4,46 +4,62 @@ using Microsoft.AspNetCore.Components;
 
 namespace Bluent.UI.Utilities;
 
-public partial class MdiTabToolbarItem : IDisposable
+public partial class MdiTabToolbarItem : IAsyncDisposable
 {
     private IMdiDocument? _document;
     [Parameter] public IMdiDocument? Document { get; set; }
+    [Inject] private IMdiService MdiService { get; set; } = default!;
+
 
     protected override void OnParametersSet()
     {
         if (_document != Document)
         {
             UnsetEvents();
-            
+
             _document = Document;
-            
+
             SetEvents();
         }
-        
+
         base.OnParametersSet();
     }
 
-    public void Dispose()
+
+    public ValueTask DisposeAsync()
     {
         UnsetEvents();
+        
+        return new ValueTask();
     }
 
     private void UnsetEvents()
     {
-        if (_document?.CommandManager is null)
-            return;
-
-        _document.CommandManager.CommandExecuted -= OnCommandExecuted;
-        _document.CommandManager.SavePointChanged -= OnSaved;
+        if (MdiService is MdiService mdiService)
+            mdiService.OnTabItemStateChanged -= OnTabItemStateChanged;
+            
+        if (_document?.CommandManager is not null)
+        {
+            _document.CommandManager.CommandExecuted -= OnCommandExecuted;
+            _document.CommandManager.SavePointChanged -= OnSaved;
+        }
     }
 
     private void SetEvents()
     {
-        if (_document?.CommandManager is null)
-            return;
+        if (MdiService is MdiService mdiService)
+            mdiService.OnTabItemStateChanged += OnTabItemStateChanged;
 
-        _document.CommandManager.CommandExecuted += OnCommandExecuted;
-        _document.CommandManager.SavePointChanged += OnSaved;
+        if (_document?.CommandManager is not null)
+        {
+            _document.CommandManager.CommandExecuted += OnCommandExecuted;
+            _document.CommandManager.SavePointChanged += OnSaved;
+        }
+    }
+
+    private void OnTabItemStateChanged(object? sender, EventArgs e)
+    {
+        StateHasChanged();
     }
 
     private void OnSaved(object? sender, EventArgs e)
